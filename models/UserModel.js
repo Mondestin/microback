ed85a51dd718437dbe412d6cde394a71f08ddbb4
Mondestin Myssie <42750483+Mondestin@users.noreply.github.com>
2024-07-04 00:08:50 +0200

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,27 +1,54 @@
 import mongoose from "mongoose";
-//user model
-const User = mongoose.Schema({
-    name:{
+
+// Define the User schema
+const userSchema = mongoose.Schema({
+    name: {
         type: String,
         required: true
     },
-    email:{
+    email: {
         type: String,
         required: true
     },
-    gender:{
+    gender: {
         type: String,
         required: true
     },
-    phone:{
+    phone: {
         type: String,
         required: true
+    },
+    cardStatus: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
     }
 });
-// convert _id to id for better use in frontend
-User.method("toJSON", function() {
+
+// Update updatedAt field on every update
+userSchema.pre('updateOne', function(next) {
+    this.update({}, { $set: { updatedAt: new Date() } });
+    next();
+});
+
+// Convert _id to id for better use in frontend
+userSchema.methods.toJSON = function() {
     const { __v, _id, ...object } = this.toObject();
     object.id = _id;
     return object;
-});
-export default mongoose.model('Users', User);
+};
+
+// Create and export the User model
+const User = mongoose.model('User', userSchema);
+export default User;
